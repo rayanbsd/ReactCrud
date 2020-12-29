55702a844661c5502bf21322b49b64b16387e2dc
rayan <rayanbsd@gmail.com>
2020-12-29 03:55:31 +0100

    add home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,24 @@
 import React, { Component } from "react";
-
+import { withStyles } from "@material-ui/core/styles";
+import LinearProgress from "@material-ui/core/LinearProgress";
+const useStyles = withStyles((theme) => ({
+  root: {
+    width: "100%",
+    "& > * + *": {
+      marginTop: theme.spacing(2),
+    },
+  },
+}));
+const classes = useStyles;
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>This Section is on devolepement</h1>
+        <h1>This Section is on devolepement Check Posts</h1>
+        <div className={classes.root}>
+          <LinearProgress />
+          <LinearProgress color="secondary" />
+        </div>
       </div>
     );
   }
